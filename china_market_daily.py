3a543fb2d0e83e245bc33b2c6ffefe7958e691f6
dalaba7046 <49179942+dalaba7046@users.noreply.github.com>
2020-09-03 16:53:18 +0800

    Update china_market_daily.py

--- china_market_daily.py
+++ china_market_daily.py
@@ -151,16 +151,13 @@ def main():
     try:
         config_path = str(Path("D:\\Oracle")) + "\\config.ini"
     	config = configparser.ConfigParser()
-		config.read(config_path, encoding="utf-8-sig")
-		price_db = config.get('section1', 'price_db')
-		log_db = config.get('section1', 'log_db')
-		#price_table = config.get('section1', 'price_table')
-		log_table = config.get('section1', 'log_table')
-		user = config.get('section1', 'user')
-		pw = config.get('section1', 'pw')
-		ip = config.get('section1', 'ip')
-		port = config.get('section1','port')
-        log_db = config.get('section1','log_db')
+	config.read(config_path, encoding="utf-8-sig")
+	price_db = config.get('section1', 'price_db')
+	log_db = config.get('section1', 'log_db')
+	user = config.get('section1', 'user')
+	pw = config.get('section1', 'pw')
+	ip = config.get('section1', 'ip')
+	port = config.get('section1','port')
         #資料庫連線:
         #價格資料表
         engine = create_engine('mysql+pymysql://'+user+':'+pw+'@'+ip+':'+port+'/'+price_db)
